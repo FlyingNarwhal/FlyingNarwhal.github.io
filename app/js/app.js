8316175a92b1ab7ea5fe23360184415ed985e79f
Joe Dahle <joe.dahle@gmail.com>
2016-03-09 17:37:53 -0700

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,27 @@
+var ViewModel = function() {
+	this.showMenu = function(){
+		$prof = $('.profile');
+		$list = $('.header-navbar-list');
+		$button = $('.section-button-link');
+
+		$prof.css('display', $prof.css("display") === 'none' ? '' : 'none');
+		$list.css('display', $list.css("display") === 'inline-block' ? '' : 'inline-block');
+		$button.css('display', $button.css("display") === 'none' ? '' : 'none');
+	};
+
+	this.comingSoon = function(){
+		$('.coming-soon').html('coming soon');
+	};
+
+	$("#blog").hover(function(){
+		$(".coming-soon").css("opacity", "1");
+	});
+	$("#blog").mouseout(function(){
+		$(".coming-soon").css("opacity", "0");
+	});
+	$("#blog").click(function(){
+		$(".coming-soon").css("opacity", "1");
+	});
+};
+
+ko.applyBindings(new ViewModel());
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+
+
+// minify html, css, js
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+// var cssmin = require('gulp-cssmin');
+// var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+ 
+gulp.task('minify', function () {
+    // gulp.src('src/**/*.css')
+    //     .pipe(cssmin())
+    //     .pipe(rename({suffix: '.min'}))
+    //     .pipe(gulp.dest('dist'));
+    gulp.src('app/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'));
+    gulp.src('app/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+var responsive = require('gulp-responsive-images');
+
+gulp.task('picture', function(){
+    gulp.src('app/images/**/*')
+        .pipe(responsive({
+            'background.jpg': [{
+                width: 2000,
+                suffix: '-2000'
+            }, {
+                width: 600,
+                suffix: '-600',
+                crop: true
+            }],
+            'profile.jpg': [{
+                width: 200,
+                suffix: '-200'
+            }]
+        }))
+        .pipe(gulp.dest('app/images'));
+});
+
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: "./app",
+        port: 3030
+    });
+
+    gulp.watch("app/**/*").on('change', browserSync.reload);
+});
+
+
+//github pages
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+ 
+gulp.task('deploy', function() {
+  return gulp.src('./app/**/*')
+    .pipe(ghPages());
+});
